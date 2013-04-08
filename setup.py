021edb26000478cea8910c8223310e75639bb817
Seth Rosenblum <seth.t.rosenblum@gmail.com>
2013-04-07 20:55:40 -0400

    Add a console_script entry point so scout can be run from command line.

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(name = "scout",
       version = "0.1",
       packages = find_packages(),
-      install_requires = ['apachelog']
+      install_requires = ['apachelog'],
+      entry_points = {'console_scripts': ['scout = scout.runner:run']}
       )
