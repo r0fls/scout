4121c787da807cda58f2b8d7ca1bab323ec5408e
Seth Rosenblum <seth.t.rosenblum@gmail.com>
2013-04-10 20:36:57 -0400

    Print out the most frequent sections since the last time we read the log.

--- scout/Monitor.py
+++ scout/Monitor.py
@@ -1,8 +1,10 @@
 import apachelog
 from urlparse import urlparse
+from collections import Counter
 
 LOG_FILE_LOCATION = '/var/log/apache2/access_log'
 APACHE_FORMAT = 'common'
+NUM_COMMON_SECTIONS = 10
 
 class Monitor(object):
     '''
@@ -25,7 +27,16 @@ class Monitor(object):
     def run_monitor(self):
         with open(LOG_FILE_LOCATION, 'r') as log_file:
             log_file.seek(self.log_file_position)
+            sections = []
             for line in log_file.readlines():
                 log_entry = self.parser.parse(line)
-                print self.get_section(log_entry)
+                sections.append(self.get_section(log_entry))
+            self.display_frequent_sections(sections)
             self.log_file_position = log_file.tell()
+
+    def display_frequent_sections(self, sections):
+        most_frequent_sections = Counter(sections).most_common(NUM_COMMON_SECTIONS)
+        print "Most Frequent Sections"
+        for section in most_frequent_sections:
+            print "%s: %s" % section
+        print "......."
