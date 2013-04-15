e860c49d400074ed2a0d8b904b26ea59fa4be62f
Seth Rosenblum <seth.t.rosenblum@gmail.com>
2013-04-14 21:13:47 -0400

    Add a LogEntry type to contain information about individual log entries.

--- scout/Monitor.py
+++ scout/Monitor.py
@@ -33,6 +33,21 @@ class Monitor(object):
         self._check_alert_state(current_time)
         self._display_frequent_sections(current_time)
 
+    def _update_log_data(self, current_time):
+        #Load any new data from access log file
+        with open(self.log_file_location, 'r') as log_file:
+            log_file.seek(self.log_file_position)
+            for line in log_file.readlines():
+                log = self.parser.parse(line)
+                log_entry = LogEntry(self._get_timestamp(log), self._get_section(log))
+                self.log_cache.append(log_entry)
+            self.log_file_position = log_file.tell()
+        #Remove any entries from cache that are out of the alerting period
+        self.log_cache = [entry for entry in self.log_cache if self._within_alert_period(entry.timestamp, current_time)]
+
+    def _get_timestamp(self, log_entry):
+        return datetime.strptime(log_entry['%t'], LOG_TIMESTAMP_FORMAT)
+
     def _get_section(self, log_entry):
         request = log_entry['%r']
         url = request.split()[1]
@@ -42,9 +57,6 @@ class Monitor(object):
             return None
         #return component after first /
         return path_components[1]
-
-    def _get_timestamp(self, log_entry):
-        return datetime.strptime(log_entry['%t'], LOG_TIMESTAMP_FORMAT)
 
     def _within_alert_period(self, timestamp, current_time):
         return current_time - timestamp < timedelta(minutes=self.alert_period)
@@ -62,24 +74,31 @@ class Monitor(object):
     def _add_alert(self, alert):
         self.alerts.append(alert)
 
-    def _update_log_data(self, current_time):
-        #Load any new data from access log file
-        with open(self.log_file_location, 'r') as log_file:
-            log_file.seek(self.log_file_position)
-            for line in log_file.readlines():
-                log_entry = self.parser.parse(line)
-                self.log_cache.append((self._get_timestamp(log_entry), self._get_section(log_entry)))
-            self.log_file_position = log_file.tell()
-        #Remove any entries from cache that are out of the alerting period
-        self.log_cache = [entry for entry in self.log_cache if self._within_alert_period(entry[0], current_time)]
-
-    def _within_polling_period(self, timestamp, current_time):
-        return current_time - timestamp < timedelta(seconds=self.period)
-
     def _display_frequent_sections(self, current_time):
-        sections = [log_entry[1] for log_entry in self.log_cache if self._within_polling_period(log_entry[0], current_time)]
+        sections = [log_entry.section for log_entry in self.log_cache if self._within_polling_period(log_entry.timestamp, current_time)]
         most_frequent_sections = Counter(sections).most_common(self.max_frequent_sections)
         print "Most Frequent Sections"
         for section in most_frequent_sections:
             print "%s: %s" % section
         print "......."
+
+    def _within_polling_period(self, timestamp, current_time):
+        return current_time - timestamp < timedelta(seconds=self.period)
+
+
+class LogEntry(object):
+    '''
+    Contains an entry from a log file
+    '''
+
+    def __init__(self, timestamp, section):
+        self._timestamp = timestamp
+        self._section = section
+
+    @property
+    def timestamp(self):
+        return self._timestamp
+
+    @property
+    def section(self):
+        return self._section
